62ec521e430ed2a81640a40f858812038e4f1ea6
kenkatou354 <kenkatou354@gmail.com>
2026-05-30 05:14:52 +0900

    [ add ] バインド機能を追加

--- Assets/Mock/Code/Scripts/Input/InputBuffer.cs
+++ Assets/Mock/Code/Scripts/Input/InputBuffer.cs
@@ -1,48 +1,52 @@
+using System.Linq;
 using TMPro;
 using UnityEngine;
+using UnityEngine.Events;
 using UnityEngine.InputSystem;
 
 [RequireComponent(typeof(PlayerInput))]
 public class InputBuffer : MonoBehaviour
 {
-    private PlayerInput _input;
-    [SerializeField]
-    private TextMeshProUGUI[] _texts;
-    InputAction action;
+
+
+    private const string MOVE_ACTION = "Move";
+    private const string LOOK_ACTION = "Look";
+    private const string JUMP_ACTION = "Jump";
+    private const string ATTACK_ACTION = "Attack";
+    private InputAction _moveAction;
+    private InputAction _lookAction;
+    private InputAction _jumpAction;
+    private InputAction _attackAction;
+    private PlayerInput _playerInput;
+    private InputView _inputView;
     private void Awake()
     {
-        _input = GetComponent<PlayerInput>();
+        _inputView = Instantiate(Resources.Load<InputView>("Prefabs/InputView"));
+        PlayerInputInitialize();
+        _inputView.Initialize(_playerInput.actions.bindings.Count());
+        _inputView.SetButtonText(ConvertStrings());
     }
 
-    private void Start()
+    private string[] ConvertStrings()
     {
-        RebindMoveUp();
+        return _playerInput.actions.bindings.Select(binding => binding.ToDisplayString()).ToArray();
     }
 
-    private void RebindMoveUp()
+    private void PlayerInputInitialize()
     {
-         action = _input.actions["Move"];
-        int bindingIndex = 1; // Up
-
-        action.Disable();
-
-        action.PerformInteractiveRebinding(bindingIndex)
-            .WithControlsHavingToMatchPath("<Keyboard>")
-            .OnComplete(operation =>
-            {
-                operation.Dispose();
-                action.Enable();
-
-                Debug.Log(action.bindings[bindingIndex].ToDisplayString());
-            })
-            .Start();
+        if(!TryGetComponent(out PlayerInput playerInput)) return;
+        _playerInput = playerInput;
+        _moveAction = _playerInput.actions[MOVE_ACTION];
+        _lookAction = _playerInput.actions[LOOK_ACTION];
+        _jumpAction = _playerInput.actions[JUMP_ACTION];
+        _attackAction = _playerInput.actions[ATTACK_ACTION];
     }
 
-    private void Update()
+    private void OnDestroy()
     {
-        for(int i = 0; i < action.bindings.Count;i++)
+        if (_inputView != null)
         {
-            _texts[i].text = action.bindings[i].ToDisplayString();
+            Destroy(_inputView.gameObject);
         }
     }
 }
