364fc2ce93697aa694be24a40cd428fa17be56de
kenkatou354 <kenkatou354@gmail.com>
2026-06-02 07:51:48 +0900

    [ add ] 入力表示UIを追加

--- Assets/Mock/Code/Scripts/Input/InputView.cs
+++ Assets/Mock/Code/Scripts/Input/InputView.cs
@@ -1,3 +1,4 @@
+using DG.Tweening;
 using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
@@ -11,25 +12,23 @@ public class InputView : ViewBase
 
     }
 
-    private void GenerateLayout()
-    {
-        GridLayoutGroup gridLayoutGroup = GetComponent<GridLayoutGroup>();
-        gridLayoutGroup.constraint = GridLayoutGroup.Constraint.FixedColumnCount;
-        gridLayoutGroup.constraintCount = 10;
-        gridLayoutGroup.childAlignment = TextAnchor.MiddleCenter;
-    }
-
     public void SetButtonText(string[] buttonTexts)
     {
         for (int i = 0; i < _buttons.Length; i++)
         {
             _buttons[i].GetComponentInChildren<TextMeshProUGUI>().text = buttonTexts[i];
+            _buttons[i].name = buttonTexts[i];
         }
     }
 
     [SerializeField]
     private Button _buttonPrefab;
     private Button[] _buttons;
+    private float _scaleMin = 0.5f;
+    private float _scaleMax = 1.00f;
+    private float _duration = 0.5f;
+
+
 
     private void GenerateButtonByActionsCount(int count)
     {
@@ -38,10 +37,15 @@ public class InputView : ViewBase
         {
             _buttons[i] = Instantiate(_buttonPrefab, transform);
             _buttons[i].onClick.AddListener(() => Debug.Log($"Button {i} pressed"));
-            _buttons[i].name = $"Button_{i}";
         }
     }
-
+    private void GenerateLayout()
+    {
+        GridLayoutGroup gridLayoutGroup = GetComponent<GridLayoutGroup>();
+        gridLayoutGroup.constraint = GridLayoutGroup.Constraint.FixedColumnCount;
+        gridLayoutGroup.constraintCount = 10;
+        gridLayoutGroup.childAlignment = TextAnchor.MiddleCenter;
+    }
     protected override void OnDestroy()
     {
         base.OnDestroy();
