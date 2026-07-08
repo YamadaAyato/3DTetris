565cd8854f1c97dd4a8f2c0a9d9ac988b6c9daa2
ayato0913a <ayato0913a@gmail.com>
2026-07-09 06:57:24 +0900

    [ fix ] 盤面上の1マス定義追加

---
+++ Assets/Scripts/1.Model/Board/BoardCellPosition.cs
@@ -0,0 +1,49 @@
+using System;
+using UnityEngine;
+
+namespace ThreeDTetris.Model
+{
+    /// <summary>
+    ///     盤面上の1マスの位置。
+    /// </summary>
+    public readonly struct BoardCellPosition : IEquatable<BoardCellPosition>
+    {
+        public BoardCellPosition(BoardFaceId faceId, int x, int y)
+        {
+            FaceId = faceId;
+            X = x;
+            Y = y;
+        }
+
+        /// <summary> 所属する面ID。 </summary>
+        public BoardFaceId FaceId { get; }
+
+        /// <summary> 面内の横座標。 </summary>
+        public int X { get; }
+
+        /// <summary> 面内の縦座標。 </summary>
+        public int Y { get; }
+
+        public bool Equals(BoardCellPosition other)
+        {
+            return FaceId.Equals(other.FaceId) && 
+                X == other.X && 
+                Y == other.Y;
+        }
+
+        public override bool Equals(object obj)
+        {
+            return obj is BoardCellPosition other && Equals(other);
+        }
+
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(FaceId, X, Y);
+        }
+
+        public override string ToString()
+        {
+            return $"{FaceId}({X},{Y})";
+        }
+    }
+}
