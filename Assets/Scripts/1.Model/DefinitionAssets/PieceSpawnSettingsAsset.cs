7f4265cdbc2c508fdc8a9ddea66ab08f5a1f81e9
ayato0913a <ayato0913a@gmail.com>
2026-07-12 05:26:01 +0900

    [ add ] スポーン設定

---
+++ Assets/Scripts/1.Model/DefinitionAssets/PieceSpawnSettingsAsset.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+
+namespace ThreeDTetris.Model
+{
+    /// <summary>
+    ///    ピースの生成位置を表す ScriptableObject。
+    /// </summary>
+    [CreateAssetMenu(fileName = "PieceSpawnSettings", menuName = "ThreeDTetris/PieceSpawnSettings")]
+    public class PieceSpawnSettingsAsset : ScriptableObject
+    {
+        /// <summary>
+        ///     設定を生成する。
+        /// </summary>
+        /// <returns> 生成されたピースの生成設定 </returns> 
+        public PieceSpawnSettings CreateSettings()
+        {
+            return new PieceSpawnSettings(_originX, _spawnHeightOffset, _initialRotation);
+        }
+
+        [SerializeField] private int _originX = 0;
+        [SerializeField] private int _spawnHeightOffset = 0;
+        [SerializeField] private PieceRotation _initialRotation = PieceRotation.Rotation0;
+    }
+}
