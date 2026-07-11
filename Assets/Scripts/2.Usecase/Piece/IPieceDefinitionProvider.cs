f449ea9ccd48c55983e6e1660f6c32f2fe87cbf3
ayato0913a <ayato0913a@gmail.com>
2026-07-11 16:33:47 +0900

    [ add ] bag方式でランダムに生成するクラスを作成

---
+++ Assets/Scripts/2.Usecase/Piece/IPieceDefinitionProvider.cs
@@ -0,0 +1,17 @@
+using ThreeDTetris.Model;
+using UnityEngine;
+
+namespace ThreeDTetris.Usecase
+{
+    /// <summary>
+    ///     次に生成されるピースの定義を提供するインターフェース。
+    /// </summary>
+    public interface IPieceDefinitionProvider
+    {
+        /// <summary>
+        ///    次に生成されるピースの定義を取得する。
+        /// </summary>
+        /// <returns> 次に生成されるピースの定義 </returns>
+        PieceDefinition GetNext();
+    }
+}
