23b18e7da615094e32d84843174dfa3c547ecd76
ayato0913a <ayato0913a@gmail.com>
2026-07-15 00:17:17 +0900

    [ fix ] 移動や形定義に合わせて

--- Assets/Scripts/1.Model/Board/DataDrivenBoardTopology.cs
+++ Assets/Scripts/1.Model/Board/DataDrivenBoardTopology.cs
@@ -5,16 +5,16 @@ namespace ThreeDTetris.Model
 {
     public class DataDrivenBoardTopology : IBoardTopology
     {
-        public DataDrivenBoardTopology(BoardTopologyDefinition definition)
+        public DataDrivenBoardTopology(BoardShapeDefinition definition)
         {
             _definition = definition ?? throw new ArgumentNullException(nameof(definition));
         }
 
-        /// <summary> 盤面の寸法。 </summary>
-        public BoardDimensions Dimensions => _definition.Dimensions;
-
         /// <summary> 盤面の面ID一覧。 </summary>
         public IReadOnlyList<BoardFaceId> FaceIds => _definition.FaceIds;
+
+        /// <summary> 盤面の初期面ID。 </summary>
+        public BoardFaceId InitialFaceId => _definition.InitialFaceId;
 
         public BoardCellPosition GetLeft(BoardCellPosition position)
         {
@@ -27,31 +27,68 @@ namespace ThreeDTetris.Model
             }
 
             // 左端なら左隣の面を返す。
-            BoardHorizontalNeighbors neighbors = _definition.GetHorizontalNeighbors(position.FaceId);
-            return new BoardCellPosition(neighbors.LeftFaceId, Dimensions.FaceWidth - 1, position.Y);
+            BoardHorizontalNeighbors neighbors = GetHorizontalNeighbors(position.FaceId);
+            int targetWidth = GetFaceWidth(neighbors.LeftFaceId);
+            return new BoardCellPosition(neighbors.LeftFaceId, targetWidth - 1, position.Y);
         }
 
         public BoardCellPosition GetRight(BoardCellPosition position)
         {
             ValidatePosition(position);
 
+            int currentFaceWidth = GetFaceWidth(position.FaceId);
+
             // 今の位置が右端でないなら同じ面の +1 の位置を返す。
-            if (position.X < Dimensions.FaceWidth - 1)
+            if (position.X < currentFaceWidth - 1)
             {
                 return new BoardCellPosition(position.FaceId, position.X + 1, position.Y);
             }
 
             // 右端なら右隣の面を返す。
-            BoardHorizontalNeighbors neighbors = _definition.GetHorizontalNeighbors(position.FaceId);
+            BoardHorizontalNeighbors neighbors = GetHorizontalNeighbors(position.FaceId);
             return new BoardCellPosition(neighbors.RightFaceId, 0, position.Y);
         }
 
         public bool ContainsFace(BoardFaceId faceId)
         {
-            return _definition.ContainsFace(faceId);
+            return _definition.DimentionsByFaceId.ContainsKey(faceId);
         }
 
-        private readonly BoardTopologyDefinition _definition;
+        public BoardDimensions GetDimensions(BoardFaceId faceId)
+        {
+            if (_definition.DimentionsByFaceId.TryGetValue(faceId, out var dimensions))
+            {
+                return dimensions;
+            }
+            else
+            {
+                throw new InvalidOperationException($"面ID {faceId} の寸法情報が定義されていません。");
+            }
+        }
+
+        public int GetFaceWidth(BoardFaceId faceId)
+        {
+            return GetDimensions(faceId).FaceWidth;
+        }
+
+        public int GetFaceHeight(BoardFaceId faceId)
+        {
+            return GetDimensions(faceId).FaceHeight;
+        }
+
+        public BoardHorizontalNeighbors GetHorizontalNeighbors(BoardFaceId faceId)
+        {
+            if (_definition.HorizontalNeighborsByFaceId.TryGetValue(faceId, out var neighbors))
+            {
+                return neighbors;
+            }
+            else
+            {
+                throw new InvalidOperationException($"面ID {faceId} の水平隣接情報が定義されていません。");
+            }
+        }
+
+        private readonly BoardShapeDefinition _definition;
 
         /// <summary>
         ///     指定された盤面位置がトポロジー上で扱える位置か検証する。
@@ -64,7 +101,9 @@ namespace ThreeDTetris.Model
                 throw new InvalidOperationException($"面ID {position.FaceId} は存在しません。");
             }
 
-            if (position.X < 0 || position.X >= Dimensions.FaceWidth)
+            int width = GetFaceWidth(position.FaceId);
+
+            if (position.X < 0 || position.X >= width)
             {
                 throw new ArgumentOutOfRangeException(nameof(position), $"X座標 {position.X} は盤面幅の範囲外です。");
             }
--- Assets/Scripts/1.Model/Board/IBoardTopology.cs
+++ Assets/Scripts/1.Model/Board/IBoardTopology.cs
@@ -7,11 +7,11 @@ namespace ThreeDTetris.Model
     /// </summary>
     public interface IBoardTopology
     {
-        /// <summary> 盤面の寸法。 </summary>
-        BoardDimensions Dimensions { get; }
-
         /// <summary> 盤面の面ID一覧。 </summary>
         IReadOnlyList<BoardFaceId> FaceIds { get; }
+
+        /// <summary> 初期操作面の面ID。 </summary>
+        BoardFaceId InitialFaceId { get; }
 
         /// <summary>
         ///     指定位置の左隣の位置を取得する。
@@ -33,5 +33,33 @@ namespace ThreeDTetris.Model
         /// <param name="faceId"> 判定する面Id </param>
         /// <returns> 存在するか </returns>
         bool ContainsFace(BoardFaceId faceId);
+
+        /// <summary>
+        ///     指定された面Idの盤面の寸法を取得する。
+        /// </summary>
+        /// <param name="faceId"> 寸法を取得する面Id </param>
+        /// <returns> 指定された面Idの盤面の寸法 </returns>
+        BoardDimensions GetDimensions(BoardFaceId faceId);
+
+        /// <summary>
+        ///     指定された面Idの盤面の幅を取得する。
+        /// </summary>
+        /// <param name="faceId"> 幅を取得する面Id </param>
+        /// <returns> 指定された面Idの盤面の幅 </returns>
+        int GetFaceWidth(BoardFaceId faceId);
+
+        /// <summary>
+        ///     指定された面Idの盤面の高さを取得する。
+        /// </summary>
+        /// <param name="faceId"> 高さを取得する面Id </param>
+        /// <returns> 指定された面Idの盤面の高さ </returns>
+        int GetFaceHeight(BoardFaceId faceId);
+
+        /// <summary>
+        ///     指定された面Idの盤面の左右隣の面Idを取得する。
+        /// </summary>
+        /// <param name="faceId"> 左右隣の面Idを取得する面Id </param>
+        /// <returns> 指定された面Idの盤面の左右隣の面Id </returns>
+        BoardHorizontalNeighbors GetHorizontalNeighbors(BoardFaceId faceId);
     }
 }
